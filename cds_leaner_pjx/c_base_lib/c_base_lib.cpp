1d0dd0f996311e9e9109d7ae73a22c0da7eeaf9a
= <1196586501@qq.com>
2021-10-17 21:20:55 +0800

    update projects
    Signed-off-by: = <1196586501@qq.com>

---
+++ cds_leaner_pjx/c_base_lib/c_base_lib.cpp
@@ -0,0 +1,22 @@
+﻿extern "C" {
+    #include "../../src/list.h"
+}
+
+#include <iostream>
+
+typedef struct {
+    char* name;
+    int age;
+    int sex;
+}Persion;
+
+typedef ListEntry PersionItem;
+typedef ListIterator PersionItor;
+typedef ListValue Persion;
+
+
+int main()
+{
+    PersionItem a = { 0 };
+    std::cout << "Hello World!\n";
+}
